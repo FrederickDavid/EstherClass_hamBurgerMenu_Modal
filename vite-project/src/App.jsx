c766862c4cbbf48e5e277201cefd88a7db20bf63
FrederickDavid <muomaifefrederick61@gmail.com>
2025-03-05 15:20:06 +0100

    Modal Function

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 const App = () => {
+  const [modal, setModal] = useState(false);
+
+  const handleClick = () => {
+    setModal(!modal);
+  };
   return (
     <>
       <Header />
+      <button onClick={handleClick}>Click Me</button>
+      {modal && <div>Modal</div>}
     </>
   );
 };
